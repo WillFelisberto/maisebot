ed7258d9ff6bc253bfc7c013d38278c3b45853f4
willfelisberto <willian_aru_@hotmail.com>
2021-01-19 09:15:37 -0300

    fix array

--- index.js
+++ index.js
@@ -30,12 +30,14 @@ function shuffle(o) {
 console.log('ligado');
 let count = 0;
 setInterval(function () {
+	if (count === maisebom.length - 1) {
+		count = 0;
+	}
 	const BotInit = () => {
-		console.log(count++);
 		Bot.post(
 			'statuses/update',
 			{
-				status: shuffle(maisebom).shift(),
+				status: maisebom[count],
 			},
 			function (err, _data, _response) {
 				console.log(err);
